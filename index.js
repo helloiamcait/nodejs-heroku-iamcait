adbf1afa2e6b5f591d9fed49cf9851caf54d63e7
Cait Martin Newnham <hello@iamcait.com>
2024-02-25 11:37:49 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + "/public"));
+/* 
+    Incase you are using mongodb atlas database uncomment below line
+    and replace "mongoAtlasUri" with your mongodb atlas uri.
+*/
+// mongoose.connect( mongoAtlasUri, {useNewUrlParser: true, useUnifiedTopology: true})
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
